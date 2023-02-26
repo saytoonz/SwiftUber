945f5c3e4cbac43a0add390d502e977cf2dcf381
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-26 16:47:56 +0000

    Location Manager For location request

--- SwiftUber/Core/Views/Representables/UberMapViewRepresentable.swift
+++ SwiftUber/Core/Views/Representables/UberMapViewRepresentable.swift
@@ -11,9 +11,11 @@ import MapKit
 struct UberMapViewRepresentable: UIViewRepresentable {
     
     let mapView = MKMapView()
+    let locationManager = LocationManager()
     
     //This is where the view to be represented in UIView is created
     func makeUIView(context: Context) -> some UIView {
+        mapView.delegate = context.coordinator
         mapView.isRotateEnabled = false
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
@@ -34,6 +36,9 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     }
 }
 
+
+
+
 extension UberMapViewRepresentable {
     
     class MapCodinator: NSObject, MKMapViewDelegate {
@@ -42,6 +47,24 @@ extension UberMapViewRepresentable {
         init(parent: UberMapViewRepresentable) {
             self.parent = parent
             super.init()
+        }
+        
+        /// This delegate method tell if the user location is updated
+        /// In here, we will use the userLocation parameter to create a boundrary/Region to zoom in
+        /// And display the user location
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(
+                center: CLLocationCoordinate2D(
+                    latitude: userLocation.coordinate.latitude,
+                    longitude: userLocation.coordinate.longitude
+                ),
+                span: MKCoordinateSpan(
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05
+                )
+            )
+            
+            parent.mapView.setRegion(region, animated: true)
         }
     }
     
