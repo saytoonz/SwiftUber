eef77e0840ff6cdd7b0226a5a35809e72d3db64a
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-26 17:43:28 +0000

    Home Search Component

---
+++ SwiftUber/Core/Home/Views/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  SwiftUber
+//
+//  Created by Sam on 26/02/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            UberMapViewRepresentable()
+                .ignoresSafeArea()
+            
+            LocationSearchActivationView()
+                .padding(.top, 72)
+            
+            MapViewActionButton()
+                .padding(.leading)
+                .padding(.top, 4)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
