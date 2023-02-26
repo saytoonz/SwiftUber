f4f234cda5aee2b6620c02236a8c65cc4a4eb62d
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-26 18:26:02 +0000

    Search Page View

---
+++ SwiftUber/Core/LocationSearch/View/LocationSearchView.swift
@@ -0,0 +1,67 @@
+//
+//  LocationSearchView.swift
+//  SwiftUber
+//
+//  Created by Sam on 26/02/2023.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    @State private var startLocationText = ""
+    @State private var destinationLocationText = ""
+    
+    var body: some View {
+        VStack{
+            //MARK: - Header View
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Rectangle()
+                        .fill(Color(.black))
+                        .frame(width: 6, height: 6)
+                }
+                VStack {
+                    TextField("Current Location", text: $startLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGroupedBackground))
+                        .padding(.trailing)
+                    
+                    TextField("Where to?", text: $destinationLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGray5))
+                        .padding(.trailing)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 40)
+            
+            Divider()
+                .padding(.vertical)
+            
+            //MARK: - List View
+            ScrollView {
+                VStack (alignment: .leading){
+                    ForEach(0 ..< 20, id: \.self) { _ in
+                        LocationSearchResultCell()
+                    }
+                }
+            }
+            
+            
+        }
+    }
+}
+
+struct LocationSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchView()
+    }
+}
