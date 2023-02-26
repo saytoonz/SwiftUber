a348ca6ec1c386ac07107fea4276d69e4f1c897a
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-26 08:17:50 +0000

    Initial Commit

---
+++ SwiftUber/SwiftUberApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUberApp.swift
+//  SwiftUber
+//
+//  Created by Sam on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
